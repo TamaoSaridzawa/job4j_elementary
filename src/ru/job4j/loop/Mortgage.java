791c8bca093e122b297d373b3b4e36a609bf740a
TamaoSaridzawa <tamaosaridzawa9@gmail.com>
2021-06-21 21:39:01 +0300

    5.7. Ипотека [# 156315 # 168840]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -2,12 +2,9 @@ package ru.job4j.loop;
 
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
-        int year = 1;
-        while (true) {
+        int year = 0;
+        while (amount > 0) {
             amount += (amount * (percent / 100));
-            if (salary > amount) {
-                break;
-            }
                 amount -= salary;
                 year++;
         }
